b1f520ce31dec92f53731ff02c2bc12abf0da85d
svillamarin27 <sebastian.villamarin@mail.escuelaing.edu.co>
2020-09-18 17:01:24 -0500

    parte 3

--- Servlet/src/main/java/edu/eci/cvds/servlet/vvServer.java
+++ Servlet/src/main/java/edu/eci/cvds/servlet/vvServer.java
@@ -60,4 +60,42 @@ public class vvServer extends HttpServlet{
             responseWriter.flush();
         }   
     }
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter responseWriter = resp.getWriter();
+        Optional<String> optName = Optional.ofNullable(req.getParameter("id"));
+        String param = optName.isPresent() && !optName.get().isEmpty() ? optName.get() : "";
+
+        if(param != "" ){
+            try{
+                int paramInt = Integer.parseInt(param);
+                if(paramInt<=200 && paramInt >=1 ){
+                    Todo todo = Service.getTodo(paramInt);
+                    List<Todo> todoList = new ArrayList<>();
+                    todoList.add(todo);
+                    responseWriter.write(Service.todosToHTMLTable(todoList));
+                    resp.setStatus(HttpServletResponse.SC_OK);
+                    resp.setContentType("text/html");
+                    responseWriter.flush();
+                }else{
+                    resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+                    responseWriter.write("No existe un item con el identificador dado.");
+                    responseWriter.flush();
+                }
+            }catch(MalformedURLException e){
+                resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+                responseWriter.write("Error interno del servidor.");
+                responseWriter.flush();
+            }catch (Exception e){
+                resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                responseWriter.write("Requerimiento inválido.");
+                responseWriter.flush();
+            }
+
+        }else{
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            responseWriter.write("Requerimiento inválido.");
+            responseWriter.flush();
+        }     
+    }
 }
